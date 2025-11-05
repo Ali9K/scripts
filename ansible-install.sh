7a08b6dbabe3c369ff81514bf8ef9a380d0c5393
alch <ali99kalbasi82@gmail.com>
2025-11-05 20:10:59 +0330

    Add install ansible script

---
+++ ansible-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This is an script for installng ansible on debian based distro's
+
+# variables
+PACKAGES_TO_INSTALL=(python3 pipx)
+
+# install
+echo "apt update..."
+sudo apt update
+
+echo "Install dependencies..."
+for pkgs in "${PACKAGES_TO_INSTALL[@]}" ; do
+	sudo apt install -y "$pkgs" ;
+done
+
+echo "Install full ansible..."
+pipx install --include-deps ansible
+
+echo "Add path"
+pipx ensurepath
+
+echo "Done! Close and reopen your terminal to use 'ansible' command."
